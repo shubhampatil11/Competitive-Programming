e3c6c1b3b1bf115e3901637e3e51b06307669bf9
Shubham Patil <34006928+fsociety88@users.noreply.github.com>
2020-01-18 00:01:05 +0530

    Create ordered_set.cpp

---
+++ STL_Data_Structures/ordered_set.cpp
@@ -0,0 +1,7 @@
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+typedef tree <int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+/*
+    order_of_key (k)
+    find_by_order(k)
+*/
