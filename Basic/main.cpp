4652ec67ac4cc54e2d2d4eb940581be46cf34bcd
Shubham Patil <34006928+fsociety88@users.noreply.github.com>
2020-01-17 23:44:50 +0530

    Create main.cpp

---
+++ Basic/main.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define LD long double
+
+const int N = 100010;
+
+int inf = 1e9;
+int mod = 1e9 + 7;
+
+signed main()
+{
+    //freopen("IN.txt", "r", stdin);
+    //freopen("OUT.txt", "w", stdout);
+
+
+    return 0;
+}
